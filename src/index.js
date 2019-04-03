169c9496e44c582c76d372400726894513016a05
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2019-04-03 09:33:48 -0400

    refactor: add global state for index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let state = {};
+window.setState = changes => {
+  state = Object.assign({}, state, changes);
+
+  ReactDOM.render(<App {...state} />, document.getElementById('root'));
+}
+
+let initialState = {
+  name: 'Ian'
+}
+
+window.setState(initialState);

0b3ae5ac27b2b91e39fb0db8347f1046a35e0ed9
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2019-04-09 15:03:14 -0400

    fix: chat input appears properly

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,7 @@ function App(props) {
         {/* Footer: renders only if viewing a chat */}
         <div className='footer'>
           <Switch>
-            <Route path='/converations/:username' component={ChatInput} />
+            <Route path='/conversations/:username' component={ChatInput} />
           </Switch>
         </div>
       </div>

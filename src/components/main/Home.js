1e0b27300d09d4a52de9fc433bad056e00226b14
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2019-04-12 17:43:59 -0400

    feat: add bubbles/msg to homepage

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -2,12 +2,36 @@ import React, { useEffect } from 'react';
 
 export default function Home(props) {
   useEffect(() => {
-    document.title = props.title
+    document.title = props.title;
   });
 
   return (
-    <div className='home-container'>
-      <button onClick={props.auth.login} className='button'>Log in with Auth0</button>
+    <div className="home-container">
+      <h1
+        style={{
+          textAlign: "center",
+          paddingTop: "20px",
+          paddingBottom: "30px"
+        }}
+      >
+        Chat with your friends, majestically.
+      </h1>
+      <div className="bubble-wrap bubble-right">
+        <div className="bubble-body bubble-right">
+          Going to the ranch at 8. Be there!
+        </div>
+      </div>
+      <div className="bubble-wrap bubble-left">
+        <div className="bubble-body bubble-left">
+          It's lit! I'll bring the grass!
+        </div>
+      </div>
+      <div style={{ textAlign: "center", paddingTop: "30px" }}>
+        Fun fact: bison can run up to 35 miles per hour.
+      </div>
+      <button onClick={props.auth.login} className="button">
+        Login / Register
+      </button>
     </div>
-  )
+  );
 }

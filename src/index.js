f58823fceb8ccfed2e26c3d01d87cc848ec8374c
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2020-08-15 16:11:40 -0400

    update packages and install tailwind

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Auth from './utils/Auth';
+import './tailwind.output.css'
 import './index.css';
 
 const auth = new Auth();

27daa187a8cdb2f1ce4657ef1c34ef0685b2eea0
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2020-08-15 18:59:43 -0400

    white nav; bison logo color change

--- src/components/main/Nav.js
+++ src/components/main/Nav.js
@@ -4,26 +4,26 @@ import { ReactComponent as Logo } from "../assets/bison.svg";
 
 function Nav(props) {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-red-500 p-4 m-2 rounded">
-      <NavLink className="flex items-center flex-shrink-0 mr-2" to="/">
-        <Logo className="fill-current h-10 w-10 mr-2" />
-        <span className="font-semibold text-white text-xl tracking-tight hidden sm:inline">BigBisonChat</span>
+    <nav className="flex items-center justify-between flex-wrap p-4 m-2 rounded">
+      <NavLink className="flex items-center flex-shrink-0 mr-2 text-red-900" to="/">
+        <Logo className="fill-current h-8 w-8 mr-2" />
+        <span className="font-semibold text-xl tracking-tight hidden sm:inline">BigBisonChat</span>
       </NavLink>
-      <ul className="flex items-baseline justify-between text-white sm:ml-auto">
+      <ul className="flex items-baseline justify-between text-red-900 sm:ml-auto">
         {props.auth.isAuthenticated() && (
           <>
             <li>
-              <NavLink className="font-semibold inline-block text-underline py-1 px-2 rounded outline-none focus:shadow-outline ml-auto" activeClassName="bg-red-600 shadow-inner" to="/new">
+              <NavLink className="font-semibold inline-block text-underline py-1 px-2 rounded outline-none focus:shadow-outline ml-auto" activeClassName="bg-red-300 shadow-inner" to="/new">
                 New
               </NavLink>
             </li>
             <li>
-              <NavLink exact className="inline-block font-semibold text-underline py-1 px-2 rounded outline-none focus:shadow-outline ml-2" activeClassName="bg-red-600 shadow-inner" to="/conversations">
+              <NavLink exact className="inline-block font-semibold text-underline py-1 px-2 rounded outline-none focus:shadow-outline ml-2" activeClassName="bg-red-300 shadow-inner" to="/conversations">
                 Convos
               </NavLink>
             </li>
             <li>
-              <button type="button" className="text-red-600 rounded bg-red-400 py-1 px-2 outline-none focus:shadow-outline ml-2" onClick={props.auth.logout}>
+              <button type="button" className="text-gray-600 rounded bg-gray-200 py-1 px-2 outline-none focus:shadow-outline ml-2" onClick={props.auth.logout}>
                 Logout
               </button>
             </li>

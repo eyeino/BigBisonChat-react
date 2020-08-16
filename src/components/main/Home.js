4428b38378e6083f5fc287c05a8a158c3fdab429
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2020-08-16 16:09:00 -0400

    home styling for tw

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -12,33 +12,18 @@ export default function Home(props) {
   }, [])
 
   return (
-    <div className="home-container">
-      <div className="text-wrapper">
-        <h1>
-          Chat with your friends, majestically.
+    <div className="flex flex-col h-screen max-w-2xl m-12 md:mt-12 md:mx-auto space-y-10">
+      <div className="flex flex-no-wrap items-center z-10">
+        <h1 className="text-5xl sm:text-6xl font-bold leading-tight">
+          Chat with your friends... <span className="text-teal-400">majestically</span>.
         </h1>
-        <div className="home-bubbles">
-          <div className="bubble-wrap bubble-right">
-            <div className="bubble-body bubble-right">
-              Going to the ranch at 8. Be there!
-            </div>
-          </div>
-          <div className="bubble-wrap bubble-left">
-            <div className="bubble-body bubble-left">
-              It's lit! I'll bring the grass!
-            </div>
-          </div>
-        </div>
-        <div className="home-bottom">
-          <div>Fun fact: bison can run</div>
-          <div>up to 35 miles per hour.</div>
-          <button onClick={props.auth.login} className="button">
-            Login / Register
-          </button>
-          <p>
-            A project by <a href='https://github.com/eyeino'>@eyeino</a>
-          </p>
-        </div>
+      </div>
+      <button onClick={props.auth.login} className="self-center bg-red-200 p-3 sm:p-5 text-red-700 font-semibold rounded-lg shadow-lg">
+        Login / Register
+      </button>
+      <div className="text-xl self-center text-center">
+        <p>Fun fact: bison can run up to 35 miles per hour.</p>
+        <p>A project by <a className="text-red-500" href='https://github.com/iannotian'>@iannotian</a></p>
       </div>
     </div>
   );

9a6b68ca702911cc77a6cb10f74d1aaec3333c1c
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2020-08-18 21:25:42 -0400

    home page color update

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -15,15 +15,14 @@ export default function Home(props) {
     <div className="flex flex-col max-w-2xl m-12 md:mt-12 md:mx-auto space-y-10">
       <div className="flex flex-no-wrap items-center z-10">
         <h1 className="text-5xl sm:text-6xl font-bold leading-tight">
-          Chat with your friends... <span className="text-teal-400">majestically</span>.
+          Chat with your friends... <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-600">majestically.</span>
         </h1>
       </div>
-      <button onClick={props.auth.login} className="self-center bg-red-200 p-3 sm:p-5 text-red-700 font-semibold rounded-lg shadow-lg">
+      <button onClick={props.auth.login} className="self-center bg-gradient-to-b from-red-400 to-red-700 p-3 sm:p-5 text-white font-semibold rounded-lg shadow-xl">
         Login / Register
       </button>
       <div className="text-xl self-center text-center">
-        <p>Fun fact: bison can run up to 35 miles per hour.</p>
-        <p>A project by <a className="text-red-500" href='https://github.com/iannotian'>@iannotian</a></p>
+        <p>A project by <a className="text-red-500 font-semibold" href='https://github.com/iannotian'>@iannotian</a></p>
       </div>
     </div>
   );

4b0ab67177749d6ff07886082fb04d87499d0d5b
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2020-09-02 12:20:29 -0400

    rename recipientbar -> recipientinput

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ConversationList from './conversations/Conversations'; // list of convos
 import { MessageList } from './chat/ChatWindow'; // list of messages in a convo
 import Callback from './auth/Callback'; // callback url for Auth0
 import { ChatInput as MessageInput } from './chat/ChatInput'; // input bar for messages in chat window
-import { RecipientBar } from './chat/RecipientBar'; // input bar for recipient in new message
+import { RecipientInput } from './chat/RecipientInput'; // input bar for recipient in new message
 import { ReactComponent as Logo } from "./assets/bison.svg";
 import { useWindowSize } from './hooks/useWindowSize';
 
@@ -25,7 +25,7 @@ function App(props) {
           <Nav auth={auth} />
           <Switch>
             <Route path='/new'>
-              <RecipientBar setRecipient={setRecipient} />
+              <RecipientInput setRecipient={setRecipient} />
             </Route>
           </Switch>
         </header>

bbec4a1dc7d3ab41e9bf95a78019d2087ff613f6
Ian MacFarlane <ian.ross.macfarlane@gmail.com>
2020-09-15 21:55:00 -0400

    prettier

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { pingServer } from '../../utils/api';
+import React, { useEffect } from "react";
+import { pingServer } from "../../utils/api";
 
 export default function Home(props) {
   useEffect(() => {
@@ -9,13 +9,16 @@ export default function Home(props) {
   useEffect(() => {
     // ping server to wake it up!
     pingServer();
-  }, [])
+  }, []);
 
   return (
     <div className="flex flex-col max-w-2xl m-12 md:mt-12 md:mx-auto space-y-10">
       <div className="flex flex-no-wrap items-center z-10">
         <h1 className="text-5xl sm:text-6xl font-bold leading-tight">
-          Chat with your friends... <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-600">majestically.</span>
+          Chat with your friends...{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-600">
+            majestically.
+          </span>
         </h1>
       </div>
       <button

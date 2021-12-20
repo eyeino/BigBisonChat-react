bb5f71471d9eb9c091ed2fed1d20a1a79e079a5a
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2021-12-19 20:44:32 -0500

    upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  purge: [
-    './src/**/*.js',
-    './src/**/*.jsx',
-    './src/**/*.html'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 }

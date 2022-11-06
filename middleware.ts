33f67afbab437e1f5dff55710447c40c886f66f5
Ian MacFarlane <1900909+iannotian@users.noreply.github.com>
2022-11-05 18:37:35 -0700

    redirect to convos if logged in

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,6 @@ import { NextResponse, NextRequest } from "next/server";
 const getIsAuthenticated = async (req: NextRequest): Promise<boolean> => {
   const url = req.nextUrl.origin + "/api/auth/me";
 
-  console.log(url);
-
   const res = await fetch(url, {
     headers: { cookie: req.headers.get("cookie") ?? "" },
   });
@@ -16,8 +14,12 @@ export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const isLoggedIn = await getIsAuthenticated(req);
 
+  if (!isLoggedIn) {
+    return;
+  }
+
   if (pathname == "/" && isLoggedIn) {
-    return NextResponse.redirect(req.nextUrl.origin + "/conversations");
+    return NextResponse.redirect(new URL("/conversations", req.url));
   }
 
   return NextResponse.next();
